cbd3fade9a19f171b330b5e5a26867c7b8bf591d
ahmed-anas <ahmed.a.tamim@gmail.com>
2016-10-14 09:55:45 +0500

    BUGGY: king refactoring

--- src/pieces/King.java
+++ src/pieces/King.java
@@ -9,13 +9,13 @@ public class King extends Piece{
 	private int x,y; //Extra variables for King class to keep a track of king's position
 	
 	//King Constructor
-	public King(String i,String p,int c,int x,int y)
+	public King(String id,String path,int color,int x,int y)
 	{
 		setx(x);
 		sety(y);
-		setId(i);
-		setPath(p);
-		setColor(c);
+		setId(id);
+		setPath(path);
+		setColor(color);
 	}
 	
 	//general value access functions
@@ -55,175 +55,143 @@ public class King extends Piece{
 	//It checks whether there is any piece of opposite color that can attack king for a given board state
 	public boolean isindanger(Cell state[][])
     {
-		
-		//Checking for attack from left,right,up and down
-    	for(int i=x+1;i<8;i++)
-    	{
-    		if(state[i][y].getpiece()==null)
-    			continue;
-    		else if(state[i][y].getpiece().getcolor()==this.getcolor())
-    			break;
-    		else
-    		{
-    			if ((state[i][y].getpiece() instanceof Rook) || (state[i][y].getpiece() instanceof Queen))
-    				return true;
-    			else
-    				break;
-    		}
-    	}
-    	for(int i=x-1;i>=0;i--)
-    	{
-    		if(state[i][y].getpiece()==null)
-    			continue;
-    		else if(state[i][y].getpiece().getcolor()==this.getcolor())
-    			break;
-    		else
-    		{
-    			if ((state[i][y].getpiece() instanceof Rook) || (state[i][y].getpiece() instanceof Queen))
-    				return true;
-    			else
-    				break;
-    		}
-    	}
-    	for(int i=y+1;i<8;i++)
-    	{
-    		if(state[x][i].getpiece()==null)
-    			continue;
-    		else if(state[x][i].getpiece().getcolor()==this.getcolor())
-    			break;
-    		else
-    		{
-    			if ((state[x][i].getpiece() instanceof Rook) || (state[x][i].getpiece() instanceof Queen))
-    				return true;
-    			else
-    				break;
-    		}
-    	}
-    	for(int i=y-1;i>=0;i--)
-    	{
-    		if(state[x][i].getpiece()==null)
-    			continue;
-    		else if(state[x][i].getpiece().getcolor()==this.getcolor())
-    			break;
-    		else
-    		{
-    			if ((state[x][i].getpiece() instanceof Rook) || (state[x][i].getpiece() instanceof Queen))
-    				return true;
-    			else
-    				break;
-    		}
-    	}
+		return isAttackedFromStraightLine(state) || isAttackedFromDiagonal(state) || isAttackedByKnight(state) || isAttackedByPawn(state) || isAttackedByKing(state);
     	
-    	//checking for attack from diagonal direction
-    	int tempx=x+1,tempy=y-1;
-		while(tempx<8&&tempy>=0)
-		{
-			if(state[tempx][tempy].getpiece()==null)
-			{
-				tempx++;
-				tempy--;
-			}
-			else if(state[tempx][tempy].getpiece().getcolor()==this.getcolor())
-				break;
-			else
-			{
-				if (state[tempx][tempy].getpiece() instanceof Bishop || state[tempx][tempy].getpiece() instanceof Queen)
-    				return true;
-    			else
-    				break;
-			}
-		}
-		tempx=x-1;tempy=y+1;
-		while(tempx>=0&&tempy<8)
-		{
-			if(state[tempx][tempy].getpiece()==null)
-			{
-				tempx--;
-				tempy++;
-			}
-			else if(state[tempx][tempy].getpiece().getcolor()==this.getcolor())
-				break;
-			else
-			{
-				if (state[tempx][tempy].getpiece() instanceof Bishop || state[tempx][tempy].getpiece() instanceof Queen)
-    				return true;
-    			else
-    				break;
-			}
-		}
-		tempx=x-1;tempy=y-1;
-		while(tempx>=0&&tempy>=0)
-		{
-			if(state[tempx][tempy].getpiece()==null)
-			{
-				tempx--;
-				tempy--;
-			}
-			else if(state[tempx][tempy].getpiece().getcolor()==this.getcolor())
-				break;
-			else
-			{
-				if (state[tempx][tempy].getpiece() instanceof Bishop || state[tempx][tempy].getpiece() instanceof Queen)
-    				return true;
-    			else
-    				break;
-			}
-		}
-		tempx=x+1;tempy=y+1;
-		while(tempx<8&&tempy<8)
-		{
-			if(state[tempx][tempy].getpiece()==null)
-			{
-				tempx++;
-				tempy++;
-			}
-			else if(state[tempx][tempy].getpiece().getcolor()==this.getcolor())
-				break;
-			else
-			{
-				if (state[tempx][tempy].getpiece() instanceof Bishop || state[tempx][tempy].getpiece() instanceof Queen)
-    				return true;
-    			else
-    				break;
-			}
-		}
-		
+    }
+	
+	private boolean locationIsEmpty(Cell[][] state, int x, int y){
+		return state[x][y].getpiece()==null;
+	}
+	
+
+	private boolean isAttackedFromStraightLine(Cell[][] state, int x, int y) {
+		return ((state[x][y].getpiece() instanceof Rook) || (state[x][y].getpiece() instanceof Queen)) && state[x][y].getpiece().getcolor()!=this.getcolor();
+	}
+
+
+	private boolean isAttackedFromStraightLine(Cell[][] state) {
+		for(int i=0;i<8;i++)
+    	{
+    		if(locationIsEmpty(state, i, y))
+    			continue;
+    		else if (isAttackedFromStraightLine(state, i, y))
+    			return true;
+    		else 
+    			break;
+    		
+    	}
+   
+    	for(int i=0;i<8;i++)
+    	{
+    		if(locationIsEmpty(state, x, i))
+    			continue;
+    		else if (isAttackedFromStraightLine(state, x, i))
+    			return true;
+    		else 
+    			break;
+    
+    	}
+    	return false;
+	}
+	
+	private boolean isAttackedByKnight(Cell[][] state, int x, int y) {
+		return state[x][y].getpiece()!=null && state[x][y].getpiece().getcolor()!=this.getcolor() && (state[x][y].getpiece() instanceof Knight);
+	}
+	private boolean isValidPosition(int x, int y)
+	{
+		return x>=0&&x<8&&y>=0&&y<8;
+	}
+	
+	private boolean isAttackedByKnight(Cell[][] state) {
 		//Checking for attack from the Knight of opposite color
 		int posx[]={x+1,x+1,x+2,x+2,x-1,x-1,x-2,x-2};
 		int posy[]={y-2,y+2,y-1,y+1,y-2,y+2,y-1,y+1};
 		for(int i=0;i<8;i++)
-			if((posx[i]>=0&&posx[i]<8&&posy[i]>=0&&posy[i]<8))
-				if(state[posx[i]][posy[i]].getpiece()!=null && state[posx[i]][posy[i]].getpiece().getcolor()!=this.getcolor() && (state[posx[i]][posy[i]].getpiece() instanceof Knight))
-				{
-					return true;
-				}
+		{
+			if(isValidPosition(posx[i], posy[i]) && isAttackedByKnight(state, posx[i], posy[i]))
+			{
+				return true;
+			}
+		}
 		
-		
-		//Checking for attack from the Pawn of opposite color
+		return false;
+	}
+	
+	private boolean isAttackedByKing(Cell[][] state, int x, int y)
+	{
+		return state[x][y].getpiece()!=null && state[x][y].getpiece().getcolor()!=this.getcolor() && (state[x][y].getpiece() instanceof King);
+	}
+	
+	private boolean isAttackedByKing(Cell[][] state)
+	{
 		int pox[]={x+1,x+1,x+1,x,x,x-1,x-1,x-1};
 		int poy[]={y-1,y+1,y,y+1,y-1,y+1,y-1,y};
+	
+		for(int i=0;i<8;i++)
 		{
-			for(int i=0;i<8;i++)
-				if((pox[i]>=0&&pox[i]<8&&poy[i]>=0&&poy[i]<8))
-					if(state[pox[i]][poy[i]].getpiece()!=null && state[pox[i]][poy[i]].getpiece().getcolor()!=this.getcolor() && (state[pox[i]][poy[i]].getpiece() instanceof King))
-					{
-						return true;
-					}
+			if(isValidPosition(pox[i], poy[i]) && isAttackedByKing(state, pox[i], poy[i]))
+			{
+				return true;
+			}
 		}
+	
+		return false;
+	}
+
+	private boolean isEnemeyPawn(Cell[][] state, int x, int y){
+		return isValidPosition(x, y) && state[x][y].getpiece()!=null&&state[x][y].getpiece().getcolor()!=this.getcolor()&&(state[x][y].getpiece() instanceof Pawn);
+	}
+	private boolean isAttackedByPawn(Cell[][] state){
+	
 		if(getcolor()==0)
 		{
-			if(x>0&&y>0&&state[x-1][y-1].getpiece()!=null&&state[x-1][y-1].getpiece().getcolor()==1&&(state[x-1][y-1].getpiece() instanceof Pawn))
-				return true;
-			if(x>0&&y<7&&state[x-1][y+1].getpiece()!=null&&state[x-1][y+1].getpiece().getcolor()==1&&(state[x-1][y+1].getpiece() instanceof Pawn))
+			if(isEnemeyPawn(state, x-1, y-1) || isEnemeyPawn(state, x-1, y+1))
 				return true;
 		}
 		else
 		{
-			if(x<7&&y>0&&state[x+1][y-1].getpiece()!=null&&state[x+1][y-1].getpiece().getcolor()==0&&(state[x+1][y-1].getpiece() instanceof Pawn))
-				return true;
-			if(x<7&&y<7&&state[x+1][y+1].getpiece()!=null&&state[x+1][y+1].getpiece().getcolor()==0&&(state[x+1][y+1].getpiece() instanceof Pawn))
+			if(isEnemeyPawn(state, x+1, y-1) || isEnemeyPawn(state, x+1, y+1))
 				return true;
 		}
-    	return false;
-    }
+		return false;
+	}
+	
+	
+
+	private boolean isAttackedFromDiagonal(Cell[][] state, int tempx, int tempy) {
+		return state[tempx][tempy].getpiece().getcolor()!=this.getcolor() && state[tempx][tempy].getpiece() instanceof Bishop || state[tempx][tempy].getpiece() instanceof Queen;
+	}
+	private boolean isAttackedFromDiagonal(Cell[][] state) {
+		
+		for(int x = 0, y = 7; x < 8 && y >=0; x++, y--)
+		{
+			if(locationIsEmpty(state, x, y))
+			{
+				continue;
+			}
+			else if(isAttackedFromDiagonal(state, x, y))
+				return true;
+			else
+			{
+				break;
+			}
+		}
+		
+		for(int x = 0, y = 0; x < 8 && y < 8; x++, y++)
+		{
+			if(locationIsEmpty(state, x, y))
+			{
+				continue;
+			}
+			else if(isAttackedFromDiagonal(state, x, y))
+				return true;
+			else
+			{
+				break;
+			}
+		}
+		
+		return false;
+	}
+
 }
