57e1c45fd770abc60bd2f37bffbcfae8402997de
ahmed-anas <ahmed.a.tamim@gmail.com>
2016-10-14 18:11:07 +0500

    knight refactoring

--- src/pieces/Knight.java
+++ src/pieces/Knight.java
@@ -12,12 +12,23 @@ import chess.Cell;
 public class Knight extends Piece{
 	
 	//Constructor
-	public Knight(String i,String p,int c)
+	public Knight(String id,String path,int color)
 	{
-		setId(i);
-		setPath(p);
-		setColor(c);
+		setId(id);
+		setPath(path);
+		setColor(color);
+		
 	}
+	
+	private boolean isValidPosition(int x, int y)
+	{
+		return x>=0&&x<8&&y>=0&&y<8;
+	}
+	
+	public boolean isValidMove(Cell[][] state, int x, int y){
+		return isValidPosition(x,y) && (state[x][y].getpiece()==null||state[x][y].getpiece().getcolor()!=this.getcolor());
+	}
+	
 	
 	//Move Function overridden
 	//There are at max 8 possible moves for a knight at any point of time.
@@ -28,11 +39,10 @@ public class Knight extends Piece{
 		int posx[]={x+1,x+1,x+2,x+2,x-1,x-1,x-2,x-2};
 		int posy[]={y-2,y+2,y-1,y+1,y-2,y+2,y-1,y+1};
 		for(int i=0;i<8;i++)
-			if((posx[i]>=0&&posx[i]<8&&posy[i]>=0&&posy[i]<8))
-				if((state[posx[i]][posy[i]].getpiece()==null||state[posx[i]][posy[i]].getpiece().getcolor()!=this.getcolor()))
-				{
-					possiblemoves.add(state[posx[i]][posy[i]]);
-				}
+			if(isValidMove(state, posx[i], posy[i]))
+			{
+				possiblemoves.add(state[posx[i]][posy[i]]);
+			}
 		return possiblemoves;
 	}
 }
